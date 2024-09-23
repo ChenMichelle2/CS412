515623ef50afdb54ed3b8e0e56bf72715a309301
Michelle <myichen@bu.edu>
2024-09-23 11:33:27 -0400

    minor changes

--- quotes/urls.py
+++ quotes/urls.py
@@ -8,8 +8,8 @@ from . import views
 # all of the URLs that are part of this app
 urlpatterns = [
     path(r'', views.quote, name="quote"),
-    path(r'quote/', views.quote, name="quote"),
-    path(r'about', views.about, name="about"),
-    path(r'show_all/', views.show_all,name="show_all")
+    path('quote/', views.quote, name="quote"),
+    path('about', views.about, name="about"),
+    path('show_all/', views.show_all,name="show_all")
     
 ]
