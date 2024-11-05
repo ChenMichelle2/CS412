d266f5ad35b09ae109da3227dffcd855c41fbc1c
Michelle <myichen@bu.edu>
2024-11-04 21:06:22 -0500

    got most of task 2 to work lol

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -19,6 +19,16 @@ class ShowAllProfilesView(ListView):
   model = Profile #the model to display
   template_name = 'mini_fb/show_all_profiles.html'
   context_object_name = 'Profiles' # how to find the data in the template file
+  def get_context_data(self, **kwargs):
+        # Get the default context data from the ListView
+        context = super().get_context_data(**kwargs)
+
+        # Add the logged-in user's profile to the context if they're authenticated
+        if self.request.user.is_authenticated:
+            user_profile = Profile.objects.get(user=self.request.user)
+            context['profile'] = user_profile
+
+        return context
 
 class ShowProfilePageView(DetailView):
   '''shows the profile of one Profile '''
@@ -46,10 +56,11 @@ class CreateStatusMessageView(LoginRequiredMixin,CreateView):
   template_name = 'mini_fb/create_status_form.html'
 
   def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    profile = Profile.objects.get(pk=self.kwargs['pk'])
-    context['profile'] = profile  # Pass profile to the template
-    return context  
+        context = super().get_context_data(**kwargs)
+        # Get the logged-in user's profile instead of relying on pk
+        profile = get_object_or_404(Profile, user=self.request.user)
+        context['profile'] = profile  # Pass profile to the template
+        return context  
   
   def get_login_url(self) -> str:
     '''Return the URL to the login page.'''
@@ -71,9 +82,13 @@ class CreateStatusMessageView(LoginRequiredMixin,CreateView):
     return super().form_valid(form)
 
   def get_success_url(self):
-    # Redirect to the profile's detail page after posting the status
-    return reverse('show_profile', args=[self.kwargs['pk']])
-  
+        # Redirect to the logged-in user's profile page after posting the status
+        profile = get_object_or_404(Profile, user=self.request.user)
+        return reverse('show_profile', kwargs={'pk': profile.pk})
+
+  def get_object(self):
+        # Fetch the logged-in user's profile instead of using pk
+        return get_object_or_404(Profile, user=self.request.user)
 class UpdateProfileView(LoginRequiredMixin, UpdateView):
   '''Updates the Profile from the form'''
   model = Profile
@@ -93,6 +108,17 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
   def get_login_url(self) -> str:
     '''Return the URL to the login page.'''
     return reverse('login')
+  
+  def get_object(self):
+        # Check if a pk is provided in the URL
+        pk = self.kwargs.get('pk', None)
+        
+        if pk:
+            # If pk is provided, get the profile associated with that pk
+            return get_object_or_404(Profile, pk=pk)
+        else:
+            # If no pk is provided, return the profile of the logged-in user
+            return get_object_or_404(Profile, user=self.request.user)
   
 class DeleteStatusMessageView(LoginRequiredMixin,DeleteView):
   '''deletes a status message'''
@@ -125,28 +151,34 @@ class UpdateStatusMessageView(LoginRequiredMixin,UpdateView):
 
 class CreateFriendView(LoginRequiredMixin, View):
   """Adds a friend relationship between two profiles based on URL parameters"""
-  def dispatch(self, request, *args, **kwargs):
-    pk = self.kwargs.get('pk')
-    other_pk = self.kwargs.get('other_pk')
-        
-    profile = get_object_or_404(Profile, pk=pk)
-    other_profile = get_object_or_404(Profile, pk=other_pk)
+  def get_object(self):
+        """Fetches the logged-in user's profile."""
+        return get_object_or_404(Profile, user=self.request.user)
 
-    # Check for self-friending
-    if profile == other_profile:
-      print("Cannot add self as a friend.")
-    else:
-      # Add friend only if it doesn't already exist
-      if profile.add_friend(other_profile):
-        print("Friendship created successfully.")
-      else:
-        print("Friendship already exists.")
+  def post(self, request, other_pk, *args, **kwargs):
+        """Handles the POST request to add a friend."""
+        # Get the logged-in user's profile
+        user_profile = self.get_object()
 
-      # Redirect back to the profile page
-      return redirect(reverse('show_profile', kwargs={'pk': pk})) 
-    def get_login_url(self) -> str:
-      '''Return the URL to the login page.'''
-      return reverse('login') 
+        # Get the profile of the user to be added as a friend
+        other_profile = get_object_or_404(Profile, pk=other_pk)
+
+        # Check for self-friending
+        if user_profile == other_profile:
+            print("Cannot add yourself as a friend.")
+        else:
+            # Add the other profile as a friend if not already a friend
+            if user_profile.add_friend(other_profile):
+                print("Friendship created successfully.")
+            else:
+                print("Friendship already exists.")
+
+        # Redirect to the logged-in user's news feed or any other desired page
+        return redirect(reverse('show_profile', kwargs={'pk': user_profile.pk}))
+  def get_login_url(self) -> str:
+        """Return the URL to the login page if the user is not authenticated."""
+        return reverse('login')
+  
     
 class ShowFriendSuggestionsView(LoginRequiredMixin, DetailView):
   """Displays friend suggestions for a single profile."""
@@ -162,11 +194,20 @@ class ShowFriendSuggestionsView(LoginRequiredMixin, DetailView):
   def get_login_url(self) -> str:
     '''Return the URL to the login page.'''
     return reverse('login')
+  
+  def get_object(self):
+    pk = self.kwargs.get('pk', None)
+        
+    if pk:
+      return get_object_or_404(Profile, pk=pk)
+    else:
+      return get_object_or_404(Profile, user=self.request.user)
 class ShowNewsFeedView(LoginRequiredMixin,DetailView):
   """Displays the news feed for a single profile."""
   model = Profile
   template_name = 'mini_fb/news_feed.html'
-  context_object_name = 'profile'
+  context_object_name = 'status_messages'
+
   def get_login_url(self) -> str:
     '''Return the URL to the login page.'''
     return reverse('login')
@@ -175,4 +216,11 @@ class ShowNewsFeedView(LoginRequiredMixin,DetailView):
     profile = self.get_object()
     context['news_feed'] = profile.get_news_feed()
     return context
+  def get_object(self):
+    pk = self.kwargs.get('pk', None)
+        
+    if pk:
+      return get_object_or_404(Profile, pk=pk)
+    else:
+      return get_object_or_404(Profile, user=self.request.user)
     
